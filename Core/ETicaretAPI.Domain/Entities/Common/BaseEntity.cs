0c5b4f0ed7267fc88ebbeab0df6a7ddf09d8cdd4
Emrah Burak <emrhbrkky@gmail.com>
2023-05-11 13:24:47 +0300

    Add project files.

---
+++ Core/ETicaretAPI.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Domain.Entities.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; } //Id yi guid veri tipinde tanımayabiliriz. Bu unique identify anlamına geliyor. Int olarakta kalabilir.
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    //Burada controller oluştururken new controller dan API Controller seçiyoruz.
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public IActionResult GetProducts()
+        {
+            return Ok();
+        }
+    }
+}
